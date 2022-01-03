5e45cb555120d0dfc8aa2d49912416c1a442cf0a
boardguy1024 <boardguy1024@gmail.com>
2022-01-03 23:10:48 +0900

    first commit

---
+++ SwiftUI_SafariTabAnimationWithGesture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_SafariTabAnimationWithGesture
+//
+//  Created by park kyung seok on 2022/01/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_SafariTabAnimationWithGesture/SwiftUI_SafariTabAnimationWithGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SafariTabAnimationWithGestureApp.swift
+//  SwiftUI_SafariTabAnimationWithGesture
+//
+//  Created by park kyung seok on 2022/01/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SafariTabAnimationWithGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
